ea7d3e758e076455efc93c2c374561580163c2b0
Kevin Hill <kevinkhill@gmail.com>
2015-06-12 20:35:17 -0700

    cleaned up column controller

--- Controllers/ColumnCharts.php
+++ Controllers/ColumnCharts.php
@@ -1,27 +1,32 @@
+<?php
 
-Route::get('column', function()
+namespace App\Controllers;
+
+class ColumnChartController extends Controller
 {
-    $finances = Lava::DataTable();
+    public function index()
+    {
+        $finances = Lava::DataTable();
 
-    $finances->addColumn('date', 'Year')
-             ->addColumn('number', 'Sales')
-             ->addColumn('number', 'Expenses')
-             ->setDateTimeFormat('Y')
-             ->addRow(array('2004', 1000, 400))
-             ->addRow(array('2005', 1170, 460))
-             ->addRow(array('2006', 660, 1120))
-             ->addRow(array('2007', 1030, 54));
+        $finances->addColumn('date', 'Year')
+                 ->addColumn('number', 'Sales')
+                 ->addColumn('number', 'Expenses')
+                 ->setDateTimeFormat('Y')
+                 ->addRow(array('2004', 1000, 400))
+                 ->addRow(array('2005', 1170, 460))
+                 ->addRow(array('2006', 660, 1120))
+                 ->addRow(array('2007', 1030, 54));
 
-    Lava::ColumnChart('Finances')
-        ->setOptions(array(
-          'datatable' => $finances,
-          'title' => 'Company Performance',
-          'titleTextStyle' => Lava::TextStyle(array(
-            'color' => '#eb6b2c',
-            'fontSize' => 14
-          ))
-        ));
+        Lava::ColumnChart('Finances')
+            ->setOptions(array(
+              'datatable' => $finances,
+              'title' => 'Company Performance',
+              'titleTextStyle' => Lava::TextStyle(array(
+                'color' => '#eb6b2c',
+                'fontSize' => 14
+              ))
+            ));
 
-    return View::make('column');
-});
-
+        return View::make('column');
+    }
+}
