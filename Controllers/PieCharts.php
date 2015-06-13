753e591f262f1b6e2e53a57ea937ce96863b1508
Kevin Hill <kevinkhill@gmail.com>
2015-06-12 20:42:36 -0700

    cleaned up pie controller

--- Controllers/PieCharts.php
+++ Controllers/PieCharts.php
@@ -1,53 +1,58 @@
+<?php
 
-Route::get('donut', function()
+namespace App\Controllers;
+
+class PieChartsController extends Controller
 {
-    $reasons = Lava::DataTable();
+    public function index()
+    {
+        $reasons = Lava::DataTable();
 
-    $reasons->addColumn('string', 'Reasons')
-            ->addColumn('number', 'Percent')
-            ->addRow(array('Check Reviews', 5))
-            ->addRow(array('Watch Trailers', 2))
-            ->addRow(array('See Actors Other Work', 4))
-            ->addRow(array('Settle Argument', 89));
+        $reasons->addColumn('string', 'Reasons')
+                ->addColumn('number', 'Percent')
+                ->addRow(array('Check Reviews', 5))
+                ->addRow(array('Watch Trailers', 2))
+                ->addRow(array('See Actors Other Work', 4))
+                ->addRow(array('Settle Argument', 89));
 
-    Lava::DonutChart('IMDB')
-        ->setOptions(array(
-          'datatable' => $reasons,
-          'title' => 'Reasons I visit IMDB'
-        ));
+        Lava::PieChart('IMDB')
+            ->setOptions(array(
+              'datatable' => $reasons,
+              'title' => 'Reasons I visit IMDB',
+              'is3D' => true,
+              'slices' => array(
+                Lava::Slice(array(
+                  'offset' => 0.2
+                )),
+                Lava::Slice(array(
+                  'offset' => 0.25
+                )),
+                Lava::Slice(array(
+                  'offset' => 0.3
+                ))
+              )
+            ));
 
-    return View::make('donut');
-});
+        return View::make('pie');
+    }
 
-Route::get('pie', function()
-{
-    $reasons = Lava::DataTable();
+    public function donut()
+    {
+        $reasons = Lava::DataTable();
 
-    $reasons->addColumn('string', 'Reasons')
-            ->addColumn('number', 'Percent')
-            ->addRow(array('Check Reviews', 5))
-            ->addRow(array('Watch Trailers', 2))
-            ->addRow(array('See Actors Other Work', 4))
-            ->addRow(array('Settle Argument', 89));
+        $reasons->addColumn('string', 'Reasons')
+                ->addColumn('number', 'Percent')
+                ->addRow(array('Check Reviews', 5))
+                ->addRow(array('Watch Trailers', 2))
+                ->addRow(array('See Actors Other Work', 4))
+                ->addRow(array('Settle Argument', 89));
 
-    Lava::PieChart('IMDB')
-        ->setOptions(array(
-          'datatable' => $reasons,
-          'title' => 'Reasons I visit IMDB',
-          'is3D' => true,
-          'slices' => array(
-            Lava::Slice(array(
-              'offset' => 0.2
-            )),
-            Lava::Slice(array(
-              'offset' => 0.25
-            )),
-            Lava::Slice(array(
-              'offset' => 0.3
-            ))
-          )
-        ));
+        Lava::DonutChart('IMDB')
+            ->setOptions(array(
+              'datatable' => $reasons,
+              'title' => 'Reasons I visit IMDB'
+            ));
 
-    return View::make('pie');
-});
-
+        return View::make('donut');
+    }
+}
