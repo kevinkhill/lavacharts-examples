ab0a972a45119f7e04e36c77d83b4bc4b0e13450
Kevin Hill <kevinkhill@gmail.com>
2015-06-12 20:38:23 -0700

    cleaned up geo controller

--- Controllers/GeoCharts.php
+++ Controllers/GeoCharts.php
@@ -1,21 +1,27 @@
+<?php
 
-Route::get('geo', function()
+namespace App\Controllers;
+
+class GeoChartController extends Controller
 {
-    $popularity = Lava::DataTable();
+    public function index()
+    {
+        $popularity = Lava::DataTable();
 
-    $popularity->addStringColumn('Country')
-               ->addNumberColumn('Popularity')
-               ->addRow(array('Germany', 200))
-               ->addRow(array('United States', 300))
-               ->addRow(array('Brazil', 400))
-               ->addRow(array('Canada', 500))
-               ->addRow(array('France', 600))
-               ->addRow(array('RU', 700));
+        $popularity->addStringColumn('Country')
+                   ->addNumberColumn('Popularity')
+                   ->addRow(array('Germany', 200))
+                   ->addRow(array('United States', 300))
+                   ->addRow(array('Brazil', 400))
+                   ->addRow(array('Canada', 500))
+                   ->addRow(array('France', 600))
+                   ->addRow(array('RU', 700));
 
-    Lava::GeoChart('Popularity')
-        ->setOptions(array(
-          'datatable' => $popularity
-        ));
+        Lava::GeoChart('Popularity')
+            ->setOptions(array(
+              'datatable' => $popularity
+            ));
 
-    return View::make('geo');
-});
+        return View::make('geo');
+    }
+}
