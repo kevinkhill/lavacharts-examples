ff1be8d9bd621c171cc95acf93c6928df623e6b9
Kevin Hill <kevinkhill@gmail.com>
2015-06-17 21:25:20 -0700

    didn't add the controller last commit, also, testing the classmap

---
+++ src/Controllers/BarChartController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Khill\Lavacharts\Examples\Controllers;
+
+class BarChartController extends Controller
+{
+    public function index()
+    {
+        $votes  = Lava::DataTable();
+
+        $votes->addStringColumn('Food')
+              ->addNumberColumn('Votes')
+              ->addRow(array('Tacos', rand(1000,5000)))
+              ->addRow(array('Salad', rand(1000,5000)))
+              ->addRow(array('Pizza', rand(1000,5000)))
+              ->addRow(array('Apples', rand(1000,5000)))
+              ->addRow(array('Fish', rand(1000,5000)));
+
+        Lava::BarChart('Votes', $votes);
+
+        return View::make('bar');
+    }
+}
